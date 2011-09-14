10fb2e47a84b5a9c830838da5bc3306321720acd
Courtney Bane <courtney@cbane.org>
2011-09-14 16:01:35 -0500

    Move sqlalchemy code from a module into main script.

--- alias-manager.py
+++ alias-manager.py
@@ -6,12 +6,12 @@ from ConfigParser import SafeConfigParser
 from configobj import ConfigObj
 from validate import Validator
 from argparse import ArgumentParser
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
 import random
 import os.path
 
 from pprint import pprint
-
-from aliasmanager import *
 
 CONFIGSPEC = {
     'database': {
@@ -28,6 +28,23 @@ CONFIGSPEC = {
         'target': 'string(default=None)',
         }
     }
+
+Base = declarative_base()
+
+class Alias(Base):
+    __tablename__ = 'aliases'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    alias = sa.Column(sa.String(255), primary_key=True, nullable=False)
+    destination = sa.Column(sa.String(255), nullable=False)
+    enabled = sa.Column(sa.Boolean, default=True)
+
+    def __init__(self, alias, destination, enabled=True):
+        self.alias = alias
+        self.destination = destination
+        self.enabled = enabled
+
+    def __repr__(self):
+        return "<Alias('%s','%s',%s)>" % (self.alias, self.destination, self.enabled)
 
 def main():
     config = ConfigObj('alias-manager.conf', configspec=CONFIGSPEC)
@@ -75,6 +92,38 @@ def main():
     session = init_db(config=config['database'])
     args.func(session, args)
         
+def init_db(config, echo=False):
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.engine.url import URL
+
+    url = URL(drivername=config['driver'],
+              username=config['user'],
+              password=config['password'],
+              host=config['host'],
+              port=config['port'],
+              database=config['dbname'])
+    if 'table' in config and config['table']:
+        change_table_name(Base.metadata, Alias, config['table'])
+    engine = sa.create_engine(url, echo=echo)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    return session
+
+def change_table_name(metadata, cls, name):
+    old_name = cls.__table__.name
+    schema = cls.__table__.schema
+
+    if name == old_name:
+        return
+
+    cls.__table__.name = name
+    if schema:
+        cls.__table__.fullname = '%s.%s' % (schema, name)
+    else:
+        cls.__table__.fullname = name
+    metadata._remove_table(old_name, schema)
+    metadata._add_table(name, schema, cls.__table__)
+
 def list_cmd(session, args):
     if args.alias:
         search = '%{0}%'.format(args.alias)
